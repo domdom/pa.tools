e0c0ac5cf19ca947a4af48d79b26702b992485ca
Dominic Ferreira <domdom@users.noreply.github.com>
2016-07-05 18:27:30 +1000

    refactor object walker

--- mod/validate.py
+++ mod/validate.py
@@ -1,41 +1,68 @@
-import os
-import os.path
-import sys
-
-import json
-
 import paths
-
 import load
 
 
-
-"""
-Validates the modinfo for a particular mod.
-"""
 def validate_modinfo(modinfo):
-    print ('Validating modinfo using rules from https://wiki.palobby.com/wiki/Planetary_Annihilation_Mod_Structure')
+    """
+    Validates the modinfo for a particular mod.
+    """
+    print('Validating modinfo using rules from https://wiki.palobby.com/wiki/Planetary_Annihilation_Mod_Structure')
     pass
 
 
-"""
-Checks for missing files.
-"""
 def validate_mod_files(mod_dir):
+    """ Checks for missing files. """
     loader = load.Loader(paths.find_media_dir())
     loader.mount('/pa', '/pa_ex1')
     loader.mount('/', mod_dir)
 
-    file_map = _walk_json(loader, '/pa/units/unit_list.json')
+    file_map = _find_missing_files(loader)
     for file, refs in file_map.items():
         if not loader.hasFile(file):
-            print (file, " not found, referenced by: ", list(refs))
-
+            print(file, "not found, referenced by:", list(refs))
 
     # file_map = _walk_json(loader, '/pa/units/unit_list.json')
     # for file, refs in file_map.items():
     #     if not loader.hasFile(file):
     #         print (file, " not found, referenced by: ", list(refs))
+
+
+def _update_missing(missing, file, referenced_by):
+    if file in missing:
+        missing[file].add(referenced_by)
+    else:
+        missing[file] = {referenced_by}
+    return missing
+
+
+def _find_missing_files(loader):
+    visited = set()
+    missing = dict()
+    file_path = '/pa/units/unit_list.json'
+    referenced_by = 'PA Engine'
+
+    _walk_json(loader, visited, missing, file_path, referenced_by)
+
+    return missing
+
+
+def _walk_json(loader, visited, missing, file_path, referenced_by):
+    visited.add(file_path)
+
+    resolved_file = loader.resolveFile(file_path)
+    if resolved_file is None:
+        _update_missing(missing, file_path, referenced_by)
+        return
+    if not file_path.endswith('.json') and not file_path.endswith('.pfx'):
+        return
+
+    obj = loader.loadJson(resolved_file)
+
+    file_list = _walk_obj(obj)
+    for file in file_list:
+        if file not in visited:
+            _walk_json(loader, visited, missing, file, file_path)
+
 
 def _parse_spec(spec_path):
     ret = set()
@@ -46,69 +73,27 @@ def _parse_spec(spec_path):
 
     return ret
 
-def _walk_obj(source, obj):
-    items = []
-    if isinstance(obj, dict):   items = obj.values()
-    elif isinstance(obj, list): items = obj
 
-    ret = {}
-    for v in items:
-        if isinstance(v, str):
-            child_ret = _parse_spec(v)
-            for v in child_ret:
-                ret[v] = ret.get(v, set()) | set([source])
+def _walk_obj(obj):
+    if isinstance(obj, str):
+        return _parse_spec(obj)
 
-        elif isinstance(v, (list, dict)):
-            child_ret = _walk_obj(source, v) 
-            for k, v in child_ret.items():
-                ret[k] = ret.get(k, set()) | v
+    specs = set()
+    if isinstance(obj, dict):
+        obj = list(obj.values())
 
-    return ret
+    if isinstance(obj, list):
+        specs = set()
+        for value in obj:
+            specs |= _walk_obj(value)
 
-def _walk_json(loader, unit_path, visited=None):
-    if visited is None: visited = set()
-
-    ret = {}
-    if unit_path in visited: return ret
-
-    visited.add(unit_path)
-    unit_path = loader.resolveFile(unit_path)
-
-    if unit_path == None: return ret
-
-    unit = loader.loadJson(unit_path)
-
-    unit_specs = _walk_obj(unit_path, unit)
-    for spec in unit_specs:
-        ret[spec] = ret.get(spec, set()) | set([unit_path])
-
-        if spec.endswith('.json') or spec.endswith('.pfx'):
-            child_ret = _walk_json(loader, spec, visited)
-            for k, v in child_ret.items():
-                ret[k] = ret.get(k, set()) | v
-
-    return ret
+    return specs
 
 
-
-
-
-
-
-
-
-validate_mod_files('../../com.pa.domdom.laser_unit_effects')
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    validate_mod_files('../../com.pa.domdom.laser_unit_effects')
 
 """
-if __name__ == '__main__':
     if len(sys.argv) != 2:
         print(sys.argv[0] + ' takes one argument, the path to the folder containing modinfo.')
     elif not os.path.isdir(sys.argv[1]):
