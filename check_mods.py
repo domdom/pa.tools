5554369f2a42381fd5c49b4998aa7c0755309d4c
Dominic Ferreira <domdom@users.noreply.github.com>
2016-07-08 13:42:45 +1000

    update checker to use json validator

--- check_mods.py
+++ check_mods.py
@@ -44,39 +44,52 @@ def _download_mods(api_mods):
             continue
 
 def _validate_mods(api_mods):
-    import glob
     issues = {}
+    import glob
     print('================== VALIDATING MODS')
     for i, mod in enumerate(api_mods):
         mod_id = mod['identifier']
-        mod_url = mod['url']
 
-        print (i, '/', len(api_mods), '-', mod_id)
+        print(i, '/', len(api_mods), '-', mod_id, end='')
 
         mod_path = os.path.join(temp_dir, mod_id)
         glob_result = glob.glob(os.path.join(mod_path, '**','modinfo.json'), recursive=True)
 
         if len(glob_result) == 0:
-            print('+++ Could not find modinfo for', mod_id)
+            print(' - FAIL: Could not find modinfo for', mod_id)
             continue
 
         modinfo_path = glob_result[0]
         mod_path = os.path.dirname(modinfo_path)
 
         issues[mod_id] = validate_mod_files(mod_path)
+        print (' - [', len(issues[mod_id].missing), '/', len(issues[mod_id].parseErrors), ']')
     print('------------------ DONE')
+    return issues
 
 
+def _print_summary(issues):
+    print('================== ISSUE SUMMARY')
+    for mod_id, modIssues in issues.items():
+        file_map = modIssues.missing
+        json_map = modIssues.parseErrors
+        if len(file_map) == 0 and len(file_map) == 0:
+            continue
+        print('=====', mod_id , '=====[', len(file_map) + len(json_map), ']')
+        print('-- FILES')
+        if len(file_map) > 0:
+            for file, refs in file_map.items():
+                print (file, 'not found, referenced by:')
+                for ref in refs:
+                    print('    ', ref)
 
-print('================== ISSUE SUMMARY')
-for mod_id, file_map in issues.items():
-    if len(file_map) > 0:
+        print('-- JSON ERRORS')
+        if len(json_map) > 0:
+            for file, errors in json_map.items():
+                for error in errors:
+                    print(error)
 
-        print('=====', mod_id , '=====[', len(file_map), ']')
-
-        for file, refs in file_map.items():
-            print (file, 'not found, referenced by:')
-            for ref in refs:
-                print('    ', ref)
+issues = _validate_mods(api_mods)
+_print_summary(issues)
 
 
