aab9f92f77c36c5b449cbc04bf2e951136265d57
domdom <domdom@users.noreply.github.com>
2016-09-18 15:46:44 +1000

    move and rename loader to pafs

---
+++ pa/__init__.py
@@ -0,0 +1,42 @@
+from os.path import isfile
+from posixpath import join, normpath
+
+
+def _join(path1, path2):
+    if path1 is None or path2 is None:
+        return None
+    return join(path1, path2.strip("/"))
+
+class pafs:
+    def __init__(self, rootPath):
+        self.mounts = [('/', rootPath)]
+
+    def mount(self, dest, path):
+        self.mounts.insert(0, (dest, path))
+
+    def unmount(self, dest):
+        for mnt, path in self.mounts:
+            if mnt == dest:
+                self.mounts.remove((mnt, path))
+                return
+
+    def resolveFile(self, path):
+        for i in range(len(self.mounts)):
+            mounts = self.mounts[i:]
+
+            file_path = path
+            for mount_point, mount_path in mounts:
+                if file_path.startswith(mount_point):
+                    file_path = _join(mount_path, file_path[len(mount_point):])
+                # if we are mounting the root, we should not propogate further
+                if mount_point == '/':
+                    break
+
+            if isfile(file_path):
+                return normpath(file_path)
+
+        return None
+
+    # returns True if any of the roots has that file
+    def hasFile(self, path):
+        return self.resolveFile(path) != None
